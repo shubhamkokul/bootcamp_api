741652a6da3e028d48f70f13eab152a8c8188cec
shubhamkokul <shubhamkokul@gmail.com>
2020-01-27 20:09:43 -0500

    Authorization JWT

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -37,5 +39,29 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
+//Encrypt password using bcrypt
+UserSchema.pre("save", async function(next) {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+//Sign JWT and Return
+UserSchema.methods.getSignedJwtTOken = function() {
+  return jwt.sign(
+    {
+      id: this._id
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRE
+    }
+  );
+};
+
+// Match user entered Password to Hashed password in DB
+UserSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
 
 module.exports = mongoose.model("User", UserSchema);
