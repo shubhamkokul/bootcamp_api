4617e0b6c4a586cb57a9617a3c954fbf203e66b6
shubhamkokul <shubhamkokul@gmail.com>
2020-01-28 21:51:00 -0500

    Updated User admin roles

---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const {
+  getUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser
+} = require("../controllers/users");
+
+const User = require("../models/User");
+
+const router = express.Router({
+  mergeParams: true
+});
+
+//Protect route middleware
+const advanceResults = require("../middleware/advanceResult");
+const { protect, authorize } = require("../middleware/auth");
+
+router.use(protect);
+router.use(authorize("admin"));
+router
+  .route("/")
+  .get(advanceResults(User), getUsers)
+  .post(createUser);
+
+router
+  .route("/:id")
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
+  
+module.exports = router;
